612d976e22114949a77145af69949b02df372549
Shivam <shivam34500@gmail.com>
2026-03-28 21:46:38 +0530

    update

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,12 @@
 import axios from 'axios';
 
 // const API_URL = 'http://localhost:5000/api';
-const API_URL = import.meta.env.VITE_API_URL ?? 'http://localhost:5000/api';
+const rawAPI = import.meta.env.VITE_API_URL ?? 'http://localhost:5000/api';
+const API_URL = rawAPI.endsWith('/api')
+  ? rawAPI
+  : rawAPI.endsWith('/')
+    ? `${rawAPI}api`
+    : `${rawAPI}/api`;
 
 const api = axios.create({
     baseURL: API_URL,
